08f1a4474645b8ef40d2795473a61f097db79ddd
Kepham123 <kennypham6@icloud.com>
2025-09-20 02:33:18 +0000

    Complete file server and answer exercise questions

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 //call back function
-serveStatic = function (req, res) {
+serveStatic = function(req,res) {
 	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
+	fs.readFile(fileName, function(err, content) {
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'text/plain' });
+			res.write('404 Not Found');
+			res.end();
+			return;
+		}
+		res.writeHead(200, {'Content-Type': 'image/jpg'});
+		res.write(content);
+		res.end();
+	});
 }
- 
-const myserver = http.createServer(serveStatic); //create a server object
-myserver.listen(80, function() {console.log("Listening on port 80" )}); 
 
+const myserver =http.createServer(serveStatic);
+myserver.listen(80, function() { console.log("Listening on Port 80"); });
